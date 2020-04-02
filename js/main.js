6d0cd5d9ad6d009b81a3e2792b0140671b10066a
sroovy <42996064+sroovy@users.noreply.github.com>
2020-04-02 18:54:19 +0900

    20200402
    ~main, story

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(function () {
+    // scroll event
+    var $section = $(".pr"); // 제어할 대상
+    var sectionInfo = []; //각ㄱ가의 섹션이 브라우저 상단으로 떨어진 거리를 담는 배열.
+
+    $section.each(function (i) {
+        var $this = $(this);
+        sectionInfo.push($this.offset().top);
+    });
+    console.log(sectionInfo);
+
+    $section.css({position:"fixed"});
+    
+    $(window).scroll(function () {
+        var sct = $(this).scrollTop();
+
+        $section.each(function (idx) {
+            var $this = $(this);
+            var $newTop = sectionInfo[idx] - sct;
+            if(sct > sectionInfo[idx]){
+                $newTop *= 0.5;
+                //$this.find(".hide").addClass("show");
+                
+            }
+            $this.css({top:$newTop});
+        })
+    })
+}); 
